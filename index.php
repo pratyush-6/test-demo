8785d244febef50fd955dbb4a428c593538c7af3
pratyush <pkpeatyush@gmail.com>
2024-02-13 12:20:27 +0530

    extra add

--- index.php
+++ index.php
@@ -1,1 +1,4 @@
 <p>prativate</p>hello
+git remote set-url origin https://pratyush-6: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@github.com/pratyush-6/test-demo.git
+git remote set-url origin https://<token>@github.com/pratyush-6/test-demo.git
+https://www.youtube.com/watch?v=W70fWpcHMzc
